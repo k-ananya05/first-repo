b16820ea2720d145dce3806e18a5081b826bde4c
k-ananya <ananyashrinidhi@gmail.com>
2025-01-14 17:00:40 +0530

    Added new folders and updated project structure

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -1,15 +1,18 @@
-from django.contrib import admin 
-from django.urls import path 
-from .views import sayHello 
-  
-urlpatterns = [ 
-    path('', sayHello, name='sayHello'), 
-]
-#define URL route for index() view
-from django.urls import path
+from django.urls import path, include
+
 from . import views
 
+from rest_framework.routers import DefaultRouter
+
+router: DefaultRouter = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
+
 urlpatterns = [
-    . . . ,
-    path('', views.index, name='index')
+    path('', views.index, name='home'),
+    path('about/', views.about, name="about"),
+    path('menu/', views.MenuItemsView.as_view(), name='menu'),
+    path('reservations/', views.reservations, name="reservations"),
+    # API paths
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='menu-item'),
+    path('booking/', include(router.urls)),
 ]
